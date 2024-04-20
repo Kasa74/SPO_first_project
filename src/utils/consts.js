dc5ba4fd06408e0ba3084fb8ccbb2c32177f6330
Nikita <niki.kudryavcev@gmail.com>
2024-04-20 22:34:03 +0300

    sf

--- src/utils/consts.js
+++ src/utils/consts.js
@@ -1,7 +1,7 @@
 export const LOGIN_ROUTE = "/login";
 export const REGISTARTION_ROUTE = "/registration";
 export const BASKET_ROUTE = "/Basket";
-export const SHOP_ROUTE = "/SPO_first_project";
+export const SHOP_ROUTE = "/";
 export const MENSWEAR_PAGE = "/Menswear";
 export const WOMENSWEAR_PAGE = "/Menswear";
 export const KIDS_PAGE = "/Menswear";
